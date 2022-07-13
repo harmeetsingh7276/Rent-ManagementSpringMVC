478bdaa28516af4112611cae97dcfc0b561872dc
harmeet <harmeetsingh7276@gmail.com>
2022-07-13 14:35:45 +0530

    Passing the data Spring way

--- RentManagementSpringMVC/src/main/java/org/rentmanagement/MainController.java
+++ RentManagementSpringMVC/src/main/java/org/rentmanagement/MainController.java
@@ -4,8 +4,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
@@ -15,9 +17,8 @@ public class MainController {
 	}
 	
 	@RequestMapping(value = "listOfTenants", method = RequestMethod.GET)
-	public String listOfTenants(HttpServletRequest request) {
-		String mobileNumber=request.getParameter("mobileNumber");
-		request.setAttribute("mobileNumber", mobileNumber);
+	public String listOfTenants(@RequestParam("mobileNumber") String mobileNumber,Model model) {
+		model.addAttribute("mobileNumber",mobileNumber);
 		return "listOfTenants";
 	}
 	
